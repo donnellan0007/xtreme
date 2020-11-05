fb12778734e449eac082dc787dba5a004465c995
Seamus Donnellan <donnellan0007@gmail.com>
2020-11-05 15:47:14 +1100

    Added a message on the header

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Trip(models.Model):
+    buyer = models.CharField(max_length=50)
+    date = models.DateField(default='')
+
+    def __str__(self):
+        return f"{self.buyer} | {self.date}"
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, purchase
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('purchase/', views.purchase, name="buy"),
 ]
